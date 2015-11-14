686d889f3562274be557959bce905826a80fa739
Gregory Thompson <gregt87@gmail.com>
2015-11-14 03:45:02 -0500

    updated to new macstack

--- server.js
+++ server.js
@@ -8,26 +8,39 @@ var privLocation = process.env.HOME + "/work/tuber/tmp/priv.pem";
 var machine1Location = {ip: "159.203.88.103", port: 8460};
 var machine2Location = {ip: "104.236.220.59", port: 8461};
 
-function server1Controller (macstack){
-  macstack.addRoute("/", function (req, res, data){
+function server1Controller (macstackApp){
+  macstackApp.addRoute("/", function (req, res, data){
     res.json({data: data});
   });
-  macstack.run();
+  macstackApp.run();
 }
 
-function server2Controller (macstack){
-  macstack.addRoute("/", function (req, res, data){
-    res.json({data: data.foo + " ALSO HELLO WORLD!!!!"});
+function server2Controller (macstackApp){
+  macstackApp.addRoute("/", function (req, res, data){
+    // var tuberClient = require("tuber-client");
+
+    // tuberClient.createConnection(privLocation, data.mac, data.mach1Loc, function (err, response, body) {
+      res.json({data: data, foo: data.foo + " ALSO HELLO WORLD!!!!"});
+    // });
   });
-  macstack.run();
+
+  macstackApp.run();
 }
 
 // console.log(server2Controller.toString());
 
-
 tuberMacstack.deployController(machine1Location, null, privLocation, {hello: "world"}, server1Controller, function (server1MacaroonSerialized) {
-  console.log("server1MacaroonSerialized = %j", server1MacaroonSerialized);
-  tuberMacstack.deployController(machine2Location, null, privLocation, {foo: "bar"}, server2Controller, function (server2MacaroonSerialized) {
+
+  var nextData = {foo: "bar", mach1Loc: machine1Location, mac: server1MacaroonSerialized};
+  //when passing macaroon "server1MacaroonSerialized"
+  
+  //need to finish adding the request portion
+  //use "privLocation" private key to "finish" the macaroon
+  
+  //add 3rd party caveat using certificate from machine2
+  //question how do i get certificate from machine 2???????
+
+  tuberMacstack.deployController(machine2Location, null, privLocation, nextData, server2Controller, function (server2MacaroonSerialized) {
     tuberClient.createConnection(privLocation, server1MacaroonSerialized, machine1Location, function (err, response, body) {
       console.log("machine 1 err = %j", err);
       console.log("machine 1 response = %j", response);
